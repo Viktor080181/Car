de88158b3bc219e5f39b9a856d27a5a43db73668
Viktor080181 <161635347+Viktor080181@users.noreply.github.com>
2024-10-24 13:04:38 +0300

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+     class Car
+    {
+        public string Mileage { get; set; }
+        public string Color { get; set; }
+        private string Car_brand;
+        private string Car_Model;
+        private string Year_of_release;
+
+    }/// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Car car1 = new Car();
+            Car car2 = new Car();
+        }
+    }
+
+   
+
+}
